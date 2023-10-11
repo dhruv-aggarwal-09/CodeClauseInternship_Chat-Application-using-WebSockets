1e4b230c0c76d65abbb1c8542716b4dc293aab3b
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-10-11 12:56:43 +0530

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const chatOutput = document.getElementById("chat-output");
+    const messageInput = document.getElementById("message");
+    const sendButton = document.getElementById("send");
+
+    // Create a WebSocket connection to the server
+    const socket = new WebSocket("ws://127.0.0.1:5500/index.html"); // Replace with your server's address
+
+    // Handle incoming messages from the server
+    socket.addEventListener("message", (event) => {
+        const message = JSON.parse(event.data);
+        chatOutput.innerHTML += `<p class="chat-message"><span class="username">${message.username}:</span> ${message.text}</p>`;
+        chatOutput.scrollTop = chatOutput.scrollHeight; // Auto-scroll to the bottom
+    });
+
+    // Send a message when the "Send" button is clicked
+    sendButton.addEventListener("click", () => {
+        const messageText = messageInput.value;
+        if (messageText.trim() !== "") {
+            const message = {
+                username: "You",
+                text: messageText,
+            };
+            socket.send(JSON.stringify(message));
+            messageInput.value = "";
+        }
+    });
+});
---
+++ server.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 3000 }); // Replace with your desired port number
+
+const clients = new Set();
+
+server.on('connection', (client) => {
+    clients.add(client);
+
+    client.on('message', (message) => {
+        // Broadcast the received message to all connected clients
+        clients.forEach((c) => {
+            if (c !== client && c.readyState === WebSocket.OPEN) {
+                c.send(message);
+            }
+        });
+    });
+
+    client.on('close', () => {
+        clients.delete(client);
+    });
+});
+
+console.log('WebSocket server is running on port 5500'); // Replace with your desired port number
